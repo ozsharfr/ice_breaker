3beb1482f23ec3138a065ac25aaa81817c002ac1
A S, Abhijith <abhias@deloitte.com>
2024-06-03 11:49:18 +0000

    ollama embedding and notebook

--- .devcontainer/postStart.sh
+++ .devcontainer/postStart.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 nohup /opt/llm_gateway/start-llm-gateway.sh --config /opt/llm_gateway/configs/model-config-ollama.yaml
+
+nohup ollama pull nomic-embed-text >> /opt/llm_gateway/ollama.log
