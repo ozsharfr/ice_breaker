14ad177539ee947fad21806f296d63435d6ebca5
A S, Abhijith <abhias@deloitte.com>
2024-06-03 12:13:12 +0000

    test file

--- .devcontainer/postStart.sh
+++ .devcontainer/postStart.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-nohup /opt/llm_gateway/start-llm-gateway.sh --config /opt/llm_gateway/configs/model-config-ollama.yaml
+nohup /opt/llm_gateway/start-llm-gateway.sh --config /opt/llm_gateway/configs/model-config-ollama.yaml >> llm_gateway.log 2>&1 &
 
-nohup ollama pull nomic-embed-text >> /opt/llm_gateway/ollama.log
+nohup ollama pull nomic-embed-text >> /opt/llm_gateway/ollama.log 2>&1 &
