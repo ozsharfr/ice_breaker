ae4175c8541c219e2eb278e2ddb1a7ccd05a2a00
A S, Abhijith <abhias@deloitte.com>
2024-06-11 10:27:12 +0000

    use new ollama feature

--- .devcontainer/postStart.sh
+++ .devcontainer/postStart.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
 nohup /opt/llm_gateway/start-llm-gateway.sh --config /opt/llm_gateway/configs/model-config-ollama.yaml >> /opt/llm_gateway/llm_gateway.log 2>&1 &
-
-ollama pull nomic-embed-text >> /opt/llm_gateway/ollama.log 2>&1
