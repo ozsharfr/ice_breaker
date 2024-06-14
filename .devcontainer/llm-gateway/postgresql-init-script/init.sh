5332a3330fce4bdac7205668ab3e343f89c36c98
A S, Abhijith <abhias@deloitte.com>
2024-06-14 06:14:47 +0000

    remove local ref to feature

--- .devcontainer/llm-gateway/postgresql-init-script/init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-if psql -lqt | cut -d \| -f 1 | grep -qw langfuse; then
-    echo "Langfuse database already exists, not seeding."
-else
-    psql -U postgres -f /docker-entrypoint-initdb.d/seed/seed_db.sql
-fi
