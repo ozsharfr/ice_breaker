1364116be10225b081092212747bf15161412213
A S, Abhijith <abhias@deloitte.com>
2024-06-28 09:30:00 +0000

    restructure model configs and startup script

--- .devcontainer/postStart.sh
+++ .devcontainer/postStart.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nohup /opt/llm_gateway/start-llm-gateway.sh --config /opt/llm_gateway/configs/model-config-ollama.yaml >> /opt/llm_gateway/llm_gateway.log 2>&1 &
+/opt/llm_gateway/start-llm-gateway.sh --config /workspaces/engacc-llm-gateway/configs/model-config-ollama.yaml >> /opt/llm_gateway/llm_gateway.log 2>&1
