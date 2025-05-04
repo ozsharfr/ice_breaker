3258bb727a1a766555432d555c103376bba48cff
Sharabi, Oz <osharabi@deloitte.co.il>
2025-05-04 09:51:48 +0000

    Update optional Annoy indexing for clustering

--- test_embed.py
+++ test_embed.py
@@ -1,6 +1,7 @@
 import openai
 import logging
 import pandas as pd
+from annoy import AnnoyIndex
 
 client = openai.OpenAI(
     api_key="sk-password",
@@ -37,8 +38,6 @@ Sore throat in adults
 Urinary problems in adults
 Wheezing in adults""". split('\n')
 
-
-
 # Generate the prompt content
 vecs = {}
 for sympthom in sympthoms:
@@ -51,6 +50,25 @@ for sympthom in sympthoms:
     data = enmbed_structure.data[0]
     vec = data.embedding
     vecs[sympthom] = vec
+
+X = df_vecs = pd.DataFrame(vecs).T    
+
+###### consider index of annoy 
+# from sklearn.cluster import KMeans
+# import numpy as np
+# kmeans = KMeans()
+# annoy_index = AnnoyIndex(df_vecs.shape[1], 'euclidean') # Choose appropriate distance
+# for j, centroid in enumerate(kmeans.cluster_centers_):
+#     annoy_index.add_item(j, centroid)
+# annoy_index.build(10) # Number of trees
+# # Approximate assignment step using Annoy
+# new_labels = np.zeros(X.shape[0], dtype=np.int32)
+# for p_index, point in enumerate(X):
+#     nearest_centroid_index = annoy_index.get_nns_by_vector(point, 1)[0]
+#     new_labels[p_index] = nearest_centroid_index
+
+
+
 
 # get 333
 print ('aa')
